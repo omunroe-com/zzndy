5f293cfafafde6c50a2bf1931c16c1b2c3e5758a
vynogradov <vynogradov@3f7c93c8-961e-0410-b270-c24f7dacc955>
2007-12-29 16:45:44 +0000

    git-svn-id: http://opensvn.csie.org/vynogradov@24 3f7c93c8-961e-0410-b270-c24f7dacc955

---
+++ league/script/league.js
@@ -0,0 +1,23 @@
+function League()	{
+	this.root = '/league/';
+	this.body = null; 
+}
+
+var docroot = '/league/';
+
+var league = new League();
+var ready = false;
+var menuHtml;
+
+var initData = {
+	menu:	[
+		{name: 'Home', href: 'home'}, {name: 'Top players', href: 'top'}
+	]
+}
+// load modules
+$.getScript(league.root + 'script/module/menu+skeleton.js');
+
+$(function()	{
+	league = new League();
+	league.body = document.getElementsByTagName('body')[0];
+})
