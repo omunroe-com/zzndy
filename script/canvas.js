854550a974f973f311a7810d3df61f85e95a20cf
vynogradov <vynogradov@3f7c93c8-961e-0410-b270-c24f7dacc955>
2008-11-05 16:30:55 +0000

    Exporting interfaces for extensions in webkit.
    Genetic algorithm implementation.
    
    git-svn-id: http://opensvn.csie.org/vynogradov@38 3f7c93c8-961e-0410-b270-c24f7dacc955

--- script/canvas.js
+++ script/canvas.js
@@ -1,18 +1,18 @@
 /*
 Copyright (c) 2008 Andriy Vynogradov
 
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
-documentation files (the "Software"), to deal in the Software without restriction, including without limitation 
-the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, 
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
 and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 
-The above copyright notice and this permission notice shall be included in all copies or substantial 
+The above copyright notice and this permission notice shall be included in all copies or substantial
 portions of the Software.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED 
-TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
-THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
-CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS 
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 IN THE SOFTWARE.
 */
 (function(){
@@ -22,6 +22,8 @@ if(!('CanvasRenderingContext2D' in this)) // WebKit
 	var ctx = document.createElement('canvas').getContext('2d')
 	var C = ctx.__proto__
 	var G = ctx.createLinearGradient(0, 0, 0, 0).__proto__
+    CanvasRenderingContext2D = {prototype:C}
+    CanvasGradient = {prototype:G}
 }
 else
 {
@@ -62,7 +64,7 @@ C.clear = function()
 {
 	if('size' in this && 'origin' in this)
 		this.clearRect(this.origin.x, this.origin.y, this.size.w, this.size.h)
-	
+
 	return this
 }
 
