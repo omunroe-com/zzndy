3a1fc6dff9994b3b9e36ff0b3b97558272e88ca1
vynogradov <vynogradov@3f7c93c8-961e-0410-b270-c24f7dacc955>
2008-12-02 14:48:27 +0000

    tokenizing was easy
    git-svn-id: http://opensvn.csie.org/vynogradov@44 3f7c93c8-961e-0410-b270-c24f7dacc955

--- unit-expressions/script/expression.js
+++ unit-expressions/script/expression.js
@@ -1,60 +1,60 @@
 /*
 
-(2+2)*10-1 --->
-    expr -
-        left expr *
-            left expr +
-                left 2
-                right 2
-            right 10
-        right 1
+ (2+2)*10-1 --->
+ expr -
+ left expr *
+ left expr +
+ left 2
+ right 2
+ right 10
+ right 1
 
-m2/s --->
-    expr /
-        left m2
-        right s
+ m2/s --->
+ expr /
+ left m2
+ right s
 
-infix to postfix
+ infix to postfix
 
-    * While there are tokens to be read:
+ * While there are tokens to be read:
 
-        * Read a token.
-        * If the token is a number, then add it to the output queue.
-        * If the token is a function token, then push it onto the stack.
-        * If the token is a function argument separator (e.g., a comma):
+ * Read a token.
+ * If the token is a number, then add it to the output queue.
+ * If the token is a function token, then push it onto the stack.
+ * If the token is a function argument separator (e.g., a comma):
 
-            * Until the topmost element of the stack is a left parenthesis, pop the element from the stack and push it onto the output queue. If no left parentheses are encountered, either the separator was misplaced or parentheses were mismatched.
+ * Until the topmost element of the stack is a left parenthesis, pop the element from the stack and push it onto the output queue. If no left parentheses are encountered, either the separator was misplaced or parentheses were mismatched.
 
-        * If the token is an operator, o1, then:
+ * If the token is an operator, o1, then:
 
-            * while there is an operator, o2, at the top of the stack, and either
+ * while there is an operator, o2, at the top of the stack, and either
 
-                    o1 is associative or left-associative and its precedence is less than (lower precedence) or equal to that of o2, or
-                    o1 is right-associative and its precedence is less than (lower precedence) that of o2,
+ o1 is associative or left-associative and its precedence is less than (lower precedence) or equal to that of o2, or
+ o1 is right-associative and its precedence is less than (lower precedence) that of o2,
 
-                pop o2 off the stack, onto the output queue;
+ pop o2 off the stack, onto the output queue;
 
-            * push o1 onto the stack.
+ * push o1 onto the stack.
 
-        * If the token is a left parenthesis, then push it onto the stack.
-        * If the token is a right parenthesis:
+ * If the token is a left parenthesis, then push it onto the stack.
+ * If the token is a right parenthesis:
 
-            * Until the token at the top of the stack is a left parenthesis, pop operators off the stack onto the output queue.
-            * Pop the left parenthesis from the stack, but not onto the output queue.
-            * If the token at the top of the stack is a function token, pop it onto the output queue.
-            * If the stack runs out without finding a left parenthesis, then there are mismatched parentheses.
+ * Until the token at the top of the stack is a left parenthesis, pop operators off the stack onto the output queue.
+ * Pop the left parenthesis from the stack, but not onto the output queue.
+ * If the token at the top of the stack is a function token, pop it onto the output queue.
+ * If the stack runs out without finding a left parenthesis, then there are mismatched parentheses.
 
-    * When there are no more tokens to read:
+ * When there are no more tokens to read:
 
-        * While there are still operator tokens in the stack:
+ * While there are still operator tokens in the stack:
 
-            * If the operator token on the top of the stack is a parenthesis, then there are mismatched parenthesis.
-            * Pop the operator onto the output queue.
+ * If the operator token on the top of the stack is a parenthesis, then there are mismatched parenthesis.
+ * Pop the operator onto the output queue.
 
-    * Exit.
+ * Exit.
 
 
-*/
+ */
 
 /**
  * Tokenize expression.
@@ -64,7 +64,14 @@ infix to postfix
  */
 function tokenize(expr)
 {
+    function isEmpty(str) {
+        return str.replace(/^\s+|\s+$/, '') != '';
+    }
 
+    return expr
+            .replace(/([() +\/*^-])/g, '@$1@')
+            .split('@')
+            .filter(isEmpty);
 }
 
 /**
@@ -82,7 +89,7 @@ function infix2postfix(infix)
  * @constructor Expression
  * @param {Array} postfix  postfix expression queue
  */
-Expression = function(postfix){
+Expression = function(postfix) {
 
 }
 
