4991e6e8250f9f823f5ecca1ed286f68eb87688c
vynogradov <vynogradov@3f7c93c8-961e-0410-b270-c24f7dacc955>
2009-05-09 21:07:03 +0000

    MVC Tetris work in progress.
    git-svn-id: http://opensvn.csie.org/vynogradov@154 3f7c93c8-961e-0410-b270-c24f7dacc955

--- script/geometry.js
+++ script/geometry.js
@@ -1,6 +1,6 @@
-(function() {
-    Point = function( x, y ) {
-        if ( x instanceof Object && 'x' in x && 'y' in y ) {
+(function($) {
+    $.Point = function( x, y ) {
+        if ( x instanceof Object && 'x' in x && 'y' in x ) {
             this.x = x.x;
             this.y = x.y;
         }
@@ -8,7 +8,7 @@
             this.x = x;
             this.y = y;
         }
-    }
+    };
 
     var P = Point.prototype;
 
@@ -28,5 +28,4 @@
         if ( arguments.length == 2 ) return this.x >= 0 && this.y >= 0 && this.x <= x1 && this.y <= y1;
         return this.x >= x1 && this.y >= y1 && this.x <= x2 && this.y <= y2;
     };
-
-}());
+}(window));
