aae7106e4e1339c0ff31586d47c563e3d011cc3b
vynogradov <vynogradov@3f7c93c8-961e-0410-b270-c24f7dacc955>
2009-05-09 15:34:16 +0000

    Filler: added keywords, favicons.
    git-svn-id: http://opensvn.csie.org/vynogradov@153 3f7c93c8-961e-0410-b270-c24f7dacc955

--- filler/script/start.js
+++ filler/script/start.js
@@ -1,121 +1,124 @@
 var canvasId = 'main-filler-canvas';
-    var canvas = document.getElementById(canvasId);
-    var coldrun = true;
-    var playerwins = 0;
-    var computerwins = 0;
+var canvas = document.getElementById(canvasId);
+var coldrun = true;
+var playerwins = 0;
+var computerwins = 0;
 
-    var filler;
+var filler;
 
-    function start()
+function report_shares( a, b )
+{
+    document.getElementById('player').innerHTML = a.toFixed(2) + '%';
+    document.getElementById('computer').innerHTML = b.toFixed(2) + '%';
+}
+
+function start()
+{
+    filler = new Filler(canvas);
+    filler.render();
+
+    if( coldrun ) init_controls();
+
+    range(7).forEach(enableButton);
+
+    report_shares(filler.logic.p1share, filler.logic.p2share);
+
+    if( playerwins == 0 && computerwins == 0 )
+        document.getElementById('message').innerHTML = '';
+    else
+        document.getElementById('message').innerHTML = playerwins + ':' + computerwins;
+
+    disableButton(filler.p1color);
+    disableButton(filler.p2color);
+}
+
+function init_controls()
+{
+    var control_holder = document.getElementById('colors');
+    var colors = filler.getColors();
+
+    var controlTmp = '<a href="#color{n}" id="color-{n}" class="color-control color-{n}" onclick="setColor({n}); return false;">{sn}</a>';
+    var styleTmp
+            = '.color-{n} {background-color: {color}; border-color: {light1} {dark1} {dark2} {light2}; color: {text-color}}\n\n'
+            + '.color-{n}:hover, .color-{n}:visited, .color-{n}:visited:hover {color: {text-color}}\n\n'
+            + '.color-{n}:hover, .color-{n}:visited:hover {background-color: {light2}}\n'
+            + '.color-{n}.disabled, .color-{n}.disabled:hover, .color-{n}.disabled:visited, .color-{n}.disabled:hover:visited {background-color:transparent;color:#888;border-color:#888 } ';
+
+    function generateControl( colorStr, n )
     {
-        filler = new Filler(canvas);
-        filler.render();
+        return controlTmp.fmt({n:n, sn:n + 1 });
+    }
 
-        if( coldrun ) init_controls();
+    function generateStyle( colorStr, n )
+    {
+        var color = new Color(colorStr);
+        var textColor = ((color.red + color.green + color.blue) / 3 + 128) % 256 < 128 ? '#262626' : '#e0e0e0';
+        return styleTmp.fmt({n:n , color:color.tint(-20), light1:color.tint(80), light2:color.tint(30), dark1:color.tint(-55), dark2:color.tint(-90),'text-color':textColor});
+    }
 
-        range(7).forEach(enableButton);
+    var style = document.createElement('style');
+    style.setAttribute('type', 'text/css');
+    style.appendChild(document.createTextNode(colors.map(generateStyle).join('\n\n')));
 
-        document.getElementById('player').innerHTML = '0.00%';
-        document.getElementById('computer').innerHTML = '0.00%';
-        if( playerwins == 0 && computerwins == 0 )
-            document.getElementById('message').innerHTML = '';
-        else
-            document.getElementById('message').innerHTML = playerwins + ':' + computerwins;
+    document.getElementsByTagName('head')[0].appendChild(style);
+
+    control_holder.innerHTML = colors.map(generateControl).join('');
+
+    window.onkeypress = function( e )
+    {
+        if( e.charCode >= 49 && e.charCode <= 55 )
+            setColor(e.charCode - 49);
+    };
+
+    coldrun = false;
+}
+
+function disableButton( i )
+{
+    var button = document.getElementById('color-' + i);
+    button.className += ' disabled';
+}
+
+function enableButton( i )
+{
+    var button = document.getElementById('color-' + i);
+    button.className = button.className.replace(/\bdisabled\b/g, '');
+}
+
+
+function setColor( n )
+{
+    var control = document.getElementById('color-' + n);
+    if( !control.className.match(/disabled/) ) {
+        enableButton(filler.p1color);
+        enableButton(filler.p2color);
+
+        filler.setColor(n);
+        filler.moveP2();
 
         disableButton(filler.p1color);
         disableButton(filler.p2color);
-    }
 
-    function init_controls()
-    {
-        var control_holder = document.getElementById('colors');
-        var colors = filler.getColors();
+        var a = filler.logic.p1share;
+        var b = filler.logic.p2share;
 
-        var controlTmp = '<a href="#color{n}" id="color-{n}" class="color-control color-{n}" onclick="setColor({n}); return false;">{sn}</a>';
-        var styleTmp
-                = '.color-{n} {background-color: {color}; border-color: {light1} {dark1} {dark2} {light2}; color: {text-color}}\n\n'
-                + '.color-{n}:hover, .color-{n}:visited, .color-{n}:visited:hover {color: {text-color}}\n\n'
-                + '.color-{n}:hover, .color-{n}:visited:hover {background-color: {light2}}\n'
-                + '.color-{n}.disabled, .color-{n}.disabled:hover, .color-{n}.disabled:visited, .color-{n}.disabled:hover:visited {background-color:transparent;color:#888;border-color:#888 } ';
+        report_shares(a, b);
 
-
-        function generateControl( colorStr, n )
-        {
-            return controlTmp.fmt({n:n, sn:n + 1 });
+        if( a > 50 ) {
+            ++playerwins;
+            gamover('You win!');
         }
-
-        function generateStyle( colorStr, n )
-        {
-            var color = new Color(colorStr);
-            var textColor = ((color.red + color.green + color.blue) / 3 + 128) % 256 < 128 ? '#262626' : '#e0e0e0';
-            return styleTmp.fmt({n:n , color:color.tint(-20), light1:color.tint(80), light2:color.tint(30), dark1:color.tint(-55), dark2:color.tint(-90),'text-color':textColor});
-        }
-
-
-        var style = document.createElement('style');
-        style.setAttribute('type', 'text/css');
-        style.appendChild(document.createTextNode(colors.map(generateStyle).join('\n\n')));
-
-        document.getElementsByTagName('head')[0].appendChild(style);
-
-        control_holder.innerHTML = colors.map(generateControl).join('');
-
-        window.onkeypress = function( e )
-        {
-            if( e.charCode >= 49 && e.charCode <= 55 )
-                setColor(e.charCode - 49);
-        };
-
-        coldrun = false;
-    }
-
-    function disableButton( i )
-    {
-        var button = document.getElementById('color-' + i);
-        button.className += ' disabled';
-    }
-
-    function enableButton( i )
-    {
-        var button = document.getElementById('color-' + i);
-        button.className = button.className.replace(/\bdisabled\b/g, '');
-    }
-
-
-    function setColor( n )
-    {
-        var control = document.getElementById('color-' + n);
-        if( !control.className.match(/disabled/) ) {
-            enableButton(filler.p1color);
-            enableButton(filler.p2color);
-
-            filler.setColor(n);
-            filler.moveP2();
-
-            disableButton(filler.p1color);
-            disableButton(filler.p2color);
-
-            var a = filler.logic.p1share;
-            var b = filler.logic.p2share;
-
-            document.getElementById('player').innerHTML = a.toFixed(2) + '%';
-            document.getElementById('computer').innerHTML = b.toFixed(2) + '%';
-
-            if( a > 50 ) {
-                ++playerwins;
-                gamover('You win!');
-            }
-            if( b > 50 ) {
-                ++computerwins;
-                gamover('You lose.');
-            }
+        if( b > 50 ) {
+            ++computerwins;
+            gamover('You lose.');
         }
     }
+}
 
-    function gamover( message )
-    {
-        range(7).forEach(disableButton);
-        document.getElementById('message').innerHTML = message + ' <a href="#restart" onclick="start(); return false;">Play again</a>';
-    }
+function gamover( message )
+{
+    range(7).forEach(disableButton);
+    document.getElementById('message').innerHTML = message + ' <a href="#restart" onclick="start(); return false;">Play again</a>';
+}
 
-    start();
+start();
