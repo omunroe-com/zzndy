fd36358b9d45d2f4484c196742ec3096b5a7e49f
vynogradov <vynogradov@3f7c93c8-961e-0410-b270-c24f7dacc955>
2009-06-12 10:02:16 +0000

    QueryBuilder prototype.
    git-svn-id: http://opensvn.csie.org/vynogradov@171 3f7c93c8-961e-0410-b270-c24f7dacc955

--- operator/script/operator.js
+++ operator/script/operator.js
@@ -1,29 +1,49 @@
 $(document).ready(function() {
 
     var placeholder = '<div class="placeholder">...</div>';
-    $('.binary').prepend(placeholder).add('.unary').append(placeholder).draggable({
-        revert: true,
-        revertDuration: 0,
-        helper: 'clone'       ,
-        activeClass: 'dragging'
-    });
 
-    $('.query').droppable({'tolerance': 'intersect',drop:function( event, object ) {
-        console.log($(this)
-                .append(prepareHelper(object.helper))
-                .find('.placeholder'))
-                .droppable({
-            'tolerance':'intersect',
-            'greedy':true,
-            drop:function( event, object )
-            {
-                console.log(this);
-                $(this).append(prepareHelper(object.helper));
-            }
-        })
-                .end();
-    }});
+    $('.binary')
+            .prepend(placeholder)
+            .add('.unary')
+            .append(placeholder)
+            .draggable({revert:true, revertDuration: 150, helper: 'clone'});
 
+    $('.query').droppable({drop:queryOnDrop, hoverClass: 'receiving', greedy: true});
+
+    function queryOnDrop()
+    {
+        var helper = arguments[1].helper;
+        $(this)
+                .append(prepareHelper(helper))
+                .find('.placeholder')
+                .droppable({drop:placeholderOnDrop, hoverClass: 'receiving', greedy: true})
+                .click(edit);
+    }
+
+    function placeholderOnDrop()
+    {
+        var helper = arguments[1].helper;
+        $(this)
+                .html(prepareHelper(helper))
+                .prepend('<span class="op">(</span>')
+                .append('<span class="op">)</span>')
+                .droppable('destroy')
+                .unbind('click', edit)
+                .find('.placeholder')
+                .droppable({drop:placeholderOnDrop, hoverClass: 'receiving', greedy: true})
+                .click(edit);
+    }
+
+    function edit()
+    {
+        $(this)
+                .droppable('destroy')
+                .unbind('click', edit)
+                .html('<input type="text" style="width: 3ex"/>')
+                .find('input')
+                .focus()
+                .keypress(function(){console.log(this.value);$(this).css('width', (this.value.length + 3) + 'ex');});
+    }
 
     function prepareHelper( helper )
     {
