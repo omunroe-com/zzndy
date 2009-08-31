eb0ec62b9a57d06a5342223e3c9b1f78647d4152
vynogradov <vynogradov@3f7c93c8-961e-0410-b270-c24f7dacc955>
2009-08-31 15:07:36 +0000

    Moved web files to directory www.
    git-svn-id: http://opensvn.csie.org/vynogradov@205 3f7c93c8-961e-0410-b270-c24f7dacc955

---
+++ filler/www/push.php
@@ -0,0 +1,40 @@
+<?php
+define(INCL, '../includes/');
+
+require_once INCL . 'FillerGame.php';
+require_once INCL . 'comet.php';
+
+
+$action = $_GET['a'];
+if($action == 'start')
+{
+    start_mp();
+}
+else if($action == 'join' && isset($_GET['k']))
+{
+    join_mp($_GET['k']);
+}
+else
+{
+    header("HTTP/1.0 404 Not Found");
+    exit();
+}
+
+function start_mp()
+{
+    $fg = new FillerGame();
+    Comet::push("top.reportCode('{$fg->getCode()}');");
+
+    $mtime = $fg->mtime();
+    do
+    {
+        sleep(1);
+        $time = $fg->mtime();
+    }
+    while(!($time>$mtime) );
+}
+
+function join_mp($code)
+{
+
+}
