2f53681c3228433e6ebeade29f496c8fcea2dcc6
zzandy <zzandy@gmail.com>
2010-06-18 13:10:38 +0300

    Added scaling for sub-jobs

--- workers/script/progress.js
+++ workers/script/progress.js
@@ -2,14 +2,25 @@
 function ProgressBar()
 {
 	this.progress = 0;
-	this.threshold = 0.01;
+	this.threshold = 0.0001;
 	this.reported = -Infinity;
+	this.scales = [];
 }
 
 ProgressBar.prototype.set = function(progress)
 {
 	if(progress > 1) progress = 1;
 	if(progress < 0) progress = 0;
+
+	if(this.scales.length >0){
+		var scale = this.scales.pop();
+		progress = scale[0] + progress * (scale[1] - scale[0]);
+		if(progress < 0 || progress > 1)
+			throw new Error('Progress Bar subdivisions failed.');
+
+		this.scales.push(scale);
+	}
+
 	var d = progress - this.reported;
 	this.progress = progress;
 
@@ -17,4 +28,15 @@ ProgressBar.prototype.set = function(progress)
 		this.reported = this.progress;
 		this.report.call(null, this.progress);
 	}
+}
+
+ProgressBar.prototype.start = function(start, end)
+{
+	if(this.scales.length>0)this.scales.pop();
+	this.scales.push([start, end]);
+}
+
+ProgressBar.prototype.end = function()
+{
+	if(this.scales.length>0)this.scales.pop();
 }
--- workers/script/work.js
+++ workers/script/work.js
@@ -8,13 +8,31 @@ progress.report = function(progress)
 }
 
 var prog = -1;
+
+var max1 = 200;
+var max2 = 100;
+
+
 function frame()
 {
-	progress.set((++prog) / 1000);
-	if(prog <= 1000)
-		setTimeout(frame, 10);
+	progress.set((++prog) / max1);
+	if(prog <= max1)
+		setTimeout(frame, 50);
+	else	{
+		prog = -1;
+		progress.start(.4, 1);
+		frame2();
+	}
+}
+
+function frame2()
+{
+	progress.set((++prog) / max2);
+	if(prog <= max2)
+		setTimeout(frame2, 50);
 	else
 		close();
 }
 
+progress.start(0, .4);
 frame();
