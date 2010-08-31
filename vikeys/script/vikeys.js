1424096502d0a249cf2d62c3f536d4be8bb375a6
zzandy <zzandy@gmail.com>
2010-08-31 12:50:31 +0300

    Handling errors and leveling up.

--- vikeys/script/vikeys.js
+++ vikeys/script/vikeys.js
@@ -6,12 +6,21 @@ function draw(c, d)
 	var deg = Math.PI / 180;
 	var r = [0, 225*deg, 180*deg, 135*deg, 270*deg, 0, 90*deg, 315*deg, 0, 45*deg];
 
-	draw = function(c, d)	{
-		return d == 5 
-			?c.fillCircle(0, 0, .4)
-			:c.save()
+	draw = function(c, d, stroke)	{
+        
+        
+			c.save()
 				.rotate(r[d])
-				.beginPath()
+                .clearRect(-1, -1, 2, 2);
+                
+		if(d == 5)  {
+            if(stroke)
+            c.strokeCircle(0, 0, .4);
+            else
+			c.fillCircle(0, 0, .4)
+        }
+        else{
+				c.beginPath()
 				.moveTo(0, -1)
 				.lineTo(.7, -.25)
 				.lineTo(.13, -.5)
@@ -19,9 +28,13 @@ function draw(c, d)
 				.lineTo(-.15, 1)
 				.lineTo(-.1, -.5)
 				.lineTo(-.7, -.25)
-				.closePath()
-				.fill()
-				.restore();
+				.closePath();
+            if(stroke)
+                c.stroke();
+            else
+				c.fill()
+        }
+		    c.restore();
 	}
 
 	draw(c,d);
@@ -35,11 +48,14 @@ this.ViKeysTrainer = function(ctx, stdout)
         var handle;
         var score = 0;
         var lvl = 0;
-        var delay = 1200;
-        var special = ['yubn.'];
+        var success = 0;
+        var maxsc = 10;
+        var delay = [1200, 1100, 1000, 1200, 1100, 1000, 900, 900, 1000, 1000];
+        var special = ['yubn.', 'yubn.', 'yubn.', '.', '.', '.', '.'];
+        var number = [1, 2, 3, 1, 2, 3, 3, 3, 4, 4];
 
         function out(msg)  {
-            stdout.innerHTML = ['Score: ', score, '; ', msg].join('');
+            stdout.innerHTML = ['Score: ', score, ', Level ',lvl+1,' [',success,'/',maxsc,']; ', msg].join('');
         }
 
         function okp(e)
@@ -49,15 +65,30 @@ this.ViKeysTrainer = function(ctx, stdout)
 
             if(dir[d].charCodeAt(0) == e.charCode)
             {
+                ctx.strokeStyle = 'white';
                 score += Math.round((5+Math.pow(lvl+1, 1.5)*5)/5)*5;
-                out('Bingo');
-                handle = window.setTimeout(f, 300);
+
+                if(++success == maxsc) {
+                    success = 0;
+                    if(++lvl >= delay.length){
+                        out('You won!');
+                    }
+                }
+
+                if(lvl < delay.length)
+                {
+                    out('Bingo');
+                    handle = window.setTimeout(f, 300);
+                }
             }
             else
             {
                 out('Wrong, <em>' + dir[d] + '</em> expected.');
+                ctx.strokeStyle = 'maroon';
                 handle = window.setTimeout(f, 300);
             }
+
+            draw(ctx, d, true);
         }
 
 		var f = function()
@@ -72,10 +103,14 @@ this.ViKeysTrainer = function(ctx, stdout)
             window.removeEventListener('keypress', okp, false);
             window.addEventListener('keypress', okp, false);
             
+            // Only show hint on levels where new keys are introduced.
+            if(lvl == 0 || lvl == 3)
             out(dir[d]);
-			ctx.clearRect(-1, -1, 2, 2);
+            else 
+            out();
+            ctx.fillStyle = "#eeeeee";
 			draw(ctx, d);
-			handle = window.setTimeout(f, delay);
+			handle = window.setTimeout(f, delay[lvl]);
 		}
 
 		f();
